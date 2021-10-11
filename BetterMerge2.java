5c649ba0b8008b7b4bc1272d8ba87a1ccfdad353
FrankGuglielmo <fdguglielmo@dons.usfca.edu>
2021-10-11 14:36:52 -0700

    BetterMerge2 goes fast now

--- BetterMerge2.java
+++ BetterMerge2.java
@@ -4,17 +4,25 @@ import java.util.Arrays;
 public class BetterMerge2 extends Main{
 
 
-    public static void merge(double[] arr, double[] left, double[] right) {
+    public static void merge(double[] arr, double[] left, double[] right, int startingIndex) {
         int ai = 0, li = 0, ri = 0;
         while (ai < ((left.length) + (right.length))) {
-            if (li == left.length)
-                arr[ai++] = right[ri++];
-            else if (ri == right.length)
-                arr[ai++] = left[li++];
-            else if (left[li] < right[ri])
-                arr[ai++] = left[li++];
-            else
-                arr[ai++] = right[ri++];
+            if (li == left.length) {
+                arr[ai + startingIndex] = right[ri++];
+                ai++;
+            }
+            else if (ri == right.length) {
+                arr[ai + startingIndex] = left[li++];
+                ai++;
+            }
+            else if (left[li] < right[ri]) {
+                arr[ai + startingIndex] = left[li++];
+                ai++;
+            }
+            else {
+                arr[ai + startingIndex] = right[ri++];
+                ai++;
+            }
         }
     }
 
@@ -45,18 +53,29 @@ public class BetterMerge2 extends Main{
     public static void sort(double [] arr){
         ArrayList<Integer> sortedRegions = findSortedRegions(arr);
 
+        double [] left;
+        double [] right;
         while(sortedRegions.size() > 2) {
-            double [] left;
-            double [] right;
+            int i = 0;
+            while (sortedRegions.size() > 2) {
+                int indexes = sortedRegions.size();
+                if (i + 2 > sortedRegions.size() - 1)
+                    break;
+                else {
+                    left = Arrays.copyOfRange(arr, sortedRegions.get(i), (sortedRegions.get(i + 1) + 1));
+                    right = Arrays.copyOfRange(arr, sortedRegions.get(i + 2), (sortedRegions.get(i + 3) + 1));
 
-            left = Arrays.copyOfRange(arr, sortedRegions.get(0), (sortedRegions.get(1) + 1));
-            right = Arrays.copyOfRange(arr, sortedRegions.get(2), (sortedRegions.get(3) + 1));
 
-            merge(arr, left, right);
+                    merge(arr, left, right, sortedRegions.get(i));
+                    sortedRegions.remove(i + 2);
+                    sortedRegions.remove(i + 1);
 
+                    i += 2;
+                }
 
-            sortedRegions.remove(2);
-            sortedRegions.remove(1);
+            }
         }
     }
+
+
 }
