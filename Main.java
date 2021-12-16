4afef49a1f923f7ed95192fc48ce734b72f5e5ef
FrankGuglielmo <fdguglielmo@dons.usfca.edu>
2021-12-16 11:56:58 -0800

    Final Version with Comments

--- Main.java
+++ Main.java
@@ -32,7 +32,6 @@ public class Main{
 
     public static void main(String [] args){
 
-
         Scanner scan = new Scanner(System.in);
         System.out.print("Input array size you would like to test: ");
         String arraySize = scan.nextLine();
